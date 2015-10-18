caf1074b51517f729883d741ffcbfeddacbdfe14
kongondo <kongondo@gmail.com>
2015-10-18 22:28:39 +0100

    Add methods getRow() and getColumn()

--- MatrixArray.php
+++ MatrixArray.php
@@ -8,10 +8,12 @@
 class MatrixArray extends WireArray {
 
 	protected $page;
+	protected $fn;
 
 	public function __construct(Page $page) {
 
-		$this->page = $page; 
+		$this->page = $page;
+
 	}
 
 	public function isValidItem($item) {
@@ -33,43 +35,99 @@ class MatrixArray extends WireArray {
 	}
 
 	/**
+	* Determine data type of passed row/column.
+	*
+	* Use by in-memory selector in getRow()/getColumn().
+	*
+	* @access private
+	* @param Page|string|int|path $rc The row/column whose values to return.
+	* @return String $title of row/column to search for.
+	*
+	*/
+	private function getValidgetRC($rc) {
+
+		//@@todo: if method called from a $pages->get?? => how to get the referenced page?
+
+		$this->fn = FieldtypeMatrix::$name;//name of the field to query
+
+		if(is_int($rc)) {
+			$p = wire('pages')->get($rc);
+			if($p && $p->id > 0) $rc = $p->title;
+		}
+
+		//if we got a path
+		elseif (preg_match('#^(\/)#', $rc)) {
+				$p = wire('pages')->get($this->sanitizer->pagePathName($rc));
+				if($p && $p->id > 0) $rc = $p->title;
+		}
+		
+		//if we got a string, we assume page title
+		elseif(is_string($rc)) $rc = $this->sanitizer->text($rc);
+
+		//if we got a Page object
+		elseif ($rc instanceof Page) $rc = $rc->title;	
+
+		return $rc;
+
+
+	}
+
+	/**
 	* Return all|limited values of the specified Row.
 	*
-	* If Row specified as int|Page object, use in-memory selector
-	* If Row specified as title, first get the 
+	* @access public
+	* @param Page|string|int|path $r The row whose values to return.
+	* @param int $limit Limit number of values to return.
+	* @param string $sort How to sort the results on fetch.
+	* @return Object $values Retrieved results.
+	*
+	*/
+	public function getRow($r, $limit = '', $sort ='') {
+
+		$r = $this->getValidgetRC($r);
+		$n = $this->fn;//name of the field to search through
+		
+		//limit
+		if($limit) $limit = (int) $limit;
+
+		//sort
+		if($sort == 'asc') $sort = 'value';
+		elseif($sort == 'desc') $sort = '-value';		
+
+		$values = wire('page')->$n->find("row=$r, limit=$limit, sort=$sort");
+
+		return $values;
+
+	}
+
+	/**
+	* Return all|limited values of the specified Column.
 	* 
 	* @access public
 	* @param Page|string|int|path $r The row whose values to return.
 	* @param int $limit Limit number of values to return.
 	* @param string $sort How to sort the results on fetch.
 	* @return Object $values Retrieved results.
+	*
 	*/
-	public function getRow($r, $limit = '', $sort='') {
+	public function getColumn($c, $limit = '', $sort ='') {
 
-		//@@todo: path
-		//@@todo: if method called for $pages->get	
+		$c = $this->getValidgetRC($c);
+		$n = $this->fn;//the name of the field to query
 
-		$n = FieldtypeMatrix::$name;
-
-		if(!ctype_digit("$r") && strlen($r)) {
-			//first get the page by name, then grab their ID
-			$r = wire('pages')->get('name=' . $this->sanitizer->pageName($r));
-			if($r and $r->id > 0) $r = $r->id;
-		}
-
-		#$r = (int) $r;
-
+		//limit
 		if($limit) $limit = (int) $limit;
+
+		//sort
 		if($sort == 'asc') $sort = 'value';
-		elseif($sort == 'desc') $sort = '-value';
+		elseif($sort == 'desc') $sort = '-value';		
 
-		
-
-
-		$values = wire('page')->$n->find("row=$r, limit=$limit, sort=$sort");
+		$values = wire('page')->$n->find("column=$c, limit=$limit, sort=$sort");
 
 		return $values;
+
 	}
+
 
 }
 
